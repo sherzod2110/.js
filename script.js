769d49e425f7f4f67bef20fcd9a252f2fe597199
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-04-07 15:57:43 +0500

    nyghnhnyt

---
+++ script.js
@@ -0,0 +1,14 @@
+alert("Xush kelibsiz")
+var firstName = prompt("Ismingiz nima?", "");
+var lastName = prompt("Familyangiz nima?", "");
+var age = prompt("Yoshingizni kiriting?", "");
+var work = prompt("Qanday ishda ishlaysiz?", "");
+alert("Malumotlar uchun raxmat");
+console.log(`
+Xush kelibsiz
+${firstName}
+${lastName}
+${age}
+${work}
+Malumotlar uchun raxmat
+`);
