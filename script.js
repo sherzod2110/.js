70f1125500500c91edd024e5a66b68f94248d949
sherzod2110 <sh.azamatugli2110@gmail.com>
2022-04-11 19:58:30 +0500

    aaaawdewdwe

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-alert("Xush kelibsiz")
-var firstName = prompt("Ismingiz nima?", "");
-var lastName = prompt("Familyangiz nima?", "");
-var age = prompt("Yoshingizni kiriting?", "");
-var work = prompt("Qanday ishda ishlaysiz?", "");
-alert("Malumotlar uchun raxmat");
-console.log(`
-Xush kelibsiz
-${firstName}
-${lastName}
-${age}
-${work}
-Malumotlar uchun raxmat
-`);
+alert("Assalomu alaykum");
+
+var enterAge =parseInt(prompt("Yoshingizni kiriting?"),10);
+
+  
+  if( enterAge >= 16){
+    var go = "Poygada qatnashishingiz mumkin"
+    console.log(go);
+    var title = document.createElement("h1")
+    go.textContent = "enterAge"
+    document.body.append(go)
+  
+  }else {
+       var stop = "Poygada qatnasha olmaysiz uzr"
+       console.log(stop);
+  var title = document.createElement("h1")
+  stop.textContent = "enterAge"
+  document.body.append(stop)
+}
